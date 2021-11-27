87e64f751597452f466ddd22aa15c02af7a82645
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2021-11-27 19:20:36 +0100

    small location update+ refactor

--- src/Monster.java
+++ src/Monster.java
@@ -9,25 +9,12 @@ public class Monster extends Unit<Unit>// alpha 2.2
 
     public void create_moster(int power)
     {
-        int[] knight = {3, 2, 1};
-        int[] rouge = {2, 3, 1};
-        int[] mage = {1, 2, 3};
-        int[][] roles = {knight, rouge, mage};
-        int[] role = roles[(int)(Math.random() * roles.length)];
-
         if (power < 1)
         {
             power = 1;
             Manager.error("minus level for monster");
         }
         level = power;
-
-        STR = role[0];
-        AG = role[1];
-        INT = role[2];
-
-
-
 
         artefact = new Item(power);
         item_change(artefact);
